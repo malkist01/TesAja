a3bcedf33772112b799e5d47a08dac2826882fab
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-08-17 18:33:30 +0700

    Update twrp_mido.mk

--- twrp_mido.mk
+++ twrp_mido.mk
@@ -1,29 +1,24 @@
+# Only the below variable(s) need to be changed!
 #
-# Copyright (C) 2022 Team Win Recovery Project
-# SPDX-License-Identifier: Apache-2.0
-#
-# Copyright (C) 2022-2023 The OrangeFox Recovery Project
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
+# Define hardware platform
+PRODUCT_PLATFORM := sdm450
 
+# Release name
 PRODUCT_RELEASE_NAME := mido
-DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
 
-# Inherit from mido device
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit any OrangeFox-specific settings
-$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_$(PRODUCT_RELEASE_NAME).mk)
-
-# Inherit from twrp common
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
-BOARD_VENDOR := Xiaomi
+PRODUCT_DEVICE := mido
+PRODUCT_NAME := twrp_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
-TARGET_VENDOR := Xiaomi
-#
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/xiaomi/mido/device.mk)
